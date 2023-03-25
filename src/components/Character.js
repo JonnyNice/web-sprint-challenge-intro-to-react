759897e46ef1c2d369d44bc5f8b005d40124de59
JonnyNice <ianholsteen@gmail.com>
2023-03-24 22:04:21 -0700

    fetch working

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,26 @@
-// Write your Character component here
+import React, { useEffect, useState } from "react";
+
+function Character() {
+
+    const [characters, setCharacters] = useState([]);
+
+    useEffect(()=>{
+        fetch('https://swapi.dev/api/people/')
+        .then((r)=>r.json())
+        .then(setCharacters);
+        console.log(characters)
+    }, []);
+
+    return (
+        <div>
+            {characters.map((character)=>(
+                <div>
+                <li>{character.name}</li>
+                <ul>{character.height}</ul>
+                </div>
+            ))}
+        </div>
+    )
+}
+
+export default Character;
